005a02979decb947697d5c9321308883a46980f1
NworahChidimma <winnernworah@gmail.com>
2025-08-26 21:58:48 +0100

    .

---
+++ bankr/app/index.jsx
@@ -0,0 +1,10 @@
+ import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View >
+      <Text style= {{color:"red", fontSize:30}}>Hello Evereyone</Text>
+      <Text style= {{color:"blue", fontSize:20, fontStyle: "italic"}}>This text is in Italics</Text>
+    </View>
+  );
+}
